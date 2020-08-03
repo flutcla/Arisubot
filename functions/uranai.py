e4dd409bbf9f9638dfa93dcb6036489b8bbb2bfb
K <k0824.l.y.r@gmail.com>
2020-08-04 00:56:36 +0900

    微修正、占いの下準備

--- functions/uranai.py
+++ functions/uranai.py
@@ -6,7 +6,7 @@ fortune = {"大吉": 30, "中吉": 20, "吉": 20, "凶": 20, "大凶": 7,
 
 special = {"スタ吉": 10, "エナ吉": 10, "鍵クロ吉": 1, "ガシャ吉": 10}
 
-comment = {"大吉": }
+# comment = {"大吉": }
 
 def omikuji(message: discord.message):
-
+    pass
